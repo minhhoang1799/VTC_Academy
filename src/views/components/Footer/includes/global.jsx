fe3552786397c6ed8e4b253dc23c1b4cc4c02975
minhhoang1799 <hoangvm1799@gmail.com>
2023-03-24 18:05:52 +0700

    commit code master

---
+++ src/views/components/Footer/includes/global.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+global.propTypes = {
+ 
+};
+
+function global(props) {
+ return (
+  <div>
+   update master
+  </div>
+ );
+}
+
+export default global;
