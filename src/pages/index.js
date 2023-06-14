2753f2772e025e86fa23d5c31fea05ef7235bd9a
minhhoang1799 <hoangvm1799@gmail.com>
2023-06-14 09:41:08 +0700

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,16 @@
 
 import Home from "./Home/Home";
-import Recruiment from "./Recruitment/Recruitment"
-import Secret from "./Secret/Secret";
 import News from "./News/News";
 import About from "./About/About";
 import Contact from "./Contact/Contact"
-import Login from "./Login/Login";
 import Error from "./Error/Error";
 
 
 const MODULE_PAGES = {
  Home,
- Recruiment,
- Secret,
  About,
  News,
  Contact,
- Login,
  Error
 }
 
--- src/views/components/Header/features/HeaderNavLink/HeaderNavLink.jsx
+++ src/views/components/Header/features/HeaderNavLink/HeaderNavLink.jsx
@@ -1,29 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link, NavLink } from 'react-router-dom';
-import './HeaderNavLink.scss'
-import { useLocation } from 'react-router-dom/cjs/react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, NavLink } from "react-router-dom";
+import "./HeaderNavLink.scss";
+import { useLocation } from "react-router-dom/cjs/react-router-dom";
 HeaderNavLink.propTypes = {
- isPath: PropTypes.array
+  isPath: PropTypes.array,
 };
 
-
 HeaderNavLink.Props = {
- isPath: []
-}
+  isPath: [],
+};
 
 function HeaderNavLink(props) {
- const {isPath} = props;
- const location = useLocation();
- return (
-  <ul>
-    {isPath.map((link) => {
-      if(link.path) return  <li key={link.key} className={link.path == location.pathname ? "active" : ""}>
-       <Link to={link.path}>{link.name}</Link>
-     </li>
-    })}
-  </ul>
- );
+  const { isPath } = props;
+  const location = useLocation();
+  return (
+    <ul>
+      {isPath.map((link) => {
+        if (link.path)
+          return (
+            <li
+              key={link.key}
+              className={link.path === location.pathname ? "active" : ""}
+            >
+              <Link to={link.path}>{link.name}</Link>
+            </li>
+          );
+      })}
+    </ul>
+  );
 }
 
 export default HeaderNavLink;
